01da20bbeac81cd03c141959770df5c98955b88b
Richard Fitkin <rfitkin90@gmail.com>
2019-03-20 21:54:44 -0700

    added data object to res.render

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
+var db = require("../models");
 
 // main page
 router.get("/", function (req, res) {
-   res.render(path.join(__dirname, "../public/index.html"));
+   db.Article.find({})
+      .then(function (data) {
+         // res.json(data);
+         console.log('data:', data);
+         res.render('index', { data: data });
+      })
+      .catch(function (err) {
+         res.json(err);
+      });
+   ;
+
 });
 
 module.exports = router;
